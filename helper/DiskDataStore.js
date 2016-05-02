9adb2f964716f4766765a566428ea101773fb878
Sean Lee <weilonge@gmail.com>
2016-05-02 20:41:30 +0800

    Remove the cache folder before initializing data cache.

--- helper/DiskDataStore.js
+++ helper/DiskDataStore.js
@@ -3,8 +3,24 @@ var Settings = require('./Settings');
 
 var DiskDataStore = {};
 
+var fs = require('fs');
+var deleteFolderRecursive = function(path) {
+  if( fs.existsSync(path) ) {
+    fs.readdirSync(path).forEach(function(file,index){
+      var curPath = path + '/' + file;
+      if(fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+
 DiskDataStore.init = function (){
   this._CACHE_PATH = Settings.get('cache_path');
+  deleteFolderRecursive(this._CACHE_PATH);
   fs.mkdirSync(this._CACHE_PATH);
 };
 
