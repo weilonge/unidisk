41e2aedbb05be53b8d224210dc9bfbf549adf6b5
Sean Lee <weilonge@gmail.com>
2018-02-15 01:51:49 +0800

    Use gulp to build web library and start web server.

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,32 @@
 var gulp = require('gulp');
 var gutil = require('gutil');
 var mocha = require('gulp-mocha');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var connect = require('gulp-connect');
 
-var TEST_PATH = 'test/*_test.js';
+const TEST_PATH = 'test/*_test.js';
+const WEB_PATH = 'web/';
 
-gulp.task('unit-test', function() {
+gulp.task('unit-test', () => {
   return gulp.src([TEST_PATH], {read: false})
     .pipe(mocha({reporter: 'list'})).on('error', gutil.log);
 });
 
 gulp.task('test', ['unit-test']);
 
-// By default, we always run 'unit-test'. But 'unit-test' relies on 'jshint'
 gulp.task('default', ['test']);
+
+gulp.task('build-web', () => {
+  return browserify('web/export.js')
+    .bundle()
+    .pipe(source('unidisk.js'))
+    .pipe(gulp.dest(WEB_PATH));
+});
+
+gulp.task('start-web', () => {
+  connect.server({
+    root: WEB_PATH,
+  });
+});
+
